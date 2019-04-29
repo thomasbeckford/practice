543843066d8c2a27fcedd8c11ca305264950e1ec
tebeck <tebeckford@gmail.com>
2019-04-29 10:30:45 -0300

    Status

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:7
-RUN mkdir /practice
-ADD . /practice
-WORKDIR /practice
+RUN mkdir /practice_docker
+ADD . /practice_docker
+WORKDIR /practice_docker
 RUN npm i
 EXPOSE 80
 CMD ["npm", "start"]
--- server.js
+++ server.js
@@ -1,21 +1,7 @@
 const express = require('express');
 const server = express();
 const PORT = 3000;
-const pg = require('pg');
-
-connectionString = 'postgres://postgres:password@db:5432/practicedocker';
-
+// const pg = require('pg');
+// pg.connect('postgres://postgres:password@localhost:5432/practicedocker');
 server.listen(PORT, () => console.log(`Server running on ${PORT}`));
-
-
-server.get('/', function (req, res, next) {
-    pg.connect(connectionString,function(err,client,done) {
-       if(err){
-           console.log("not able to get connection "+ err);
-           res.status(400).send(err);
-       } 
-       res.status(200).json({"status": "Connected"});
-    });
-});
-
-
+server.get('/', (req, res) => res.status(200).send('hello'));
